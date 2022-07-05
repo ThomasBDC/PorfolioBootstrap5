61f6961b493783433b7090ea91109181c1d303e7
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-07-05 11:01:51 +0200

    add temoignage block and scrolltop btn

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const navbar = document.getElementById("navbarSite");
+const scrollTopButton = document.getElementById("scroll-top-button");
 
 window.onscroll=function(){
     if(window.pageYOffset>200){
         navbar.classList.remove("bg-transparent", "navbar-dark");
         navbar.classList.add("bg-light", "navbar-light");
+        scrollTopButton.classList.add("show");
     }
     else{
         navbar.classList.add("bg-transparent", "navbar-dark");
         navbar.classList.remove("bg-light", "navbar-light");
+        scrollTopButton.classList.remove("show");
     }
 }
